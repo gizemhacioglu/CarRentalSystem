622882461f36a2d6bc8e977ff6e91a6e59d78a96
gizemhacioglu <64923120+gizemhacioglu@users.noreply.github.com>
2021-04-07 00:45:51 +0300

    Initial Commit

---
+++ src/Algorithms/MergeSort.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+public class MergeSort {
+    public static void mergeSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input,mid,end);
+        merge(input,start,mid,end);
+    }
+    public static void merge(int[] input,int start,int mid,int end){
+        if(input[mid-1]<= input[mid]){
+            return;
+        }
+        int i = start;
+        int j = mid;
+        int tempIndex=0;
+        int[] temp = new int[end-start];
+        while(i<start && j<end){
+            temp[tempIndex++]= input[i]<=input[j] ? input[i++] : input[j++];
+
+        }
+
+
+        System.arraycopy(input,i,input,start+tempIndex,mid-1);
+        System.arraycopy(temp,0,input,start,tempIndex);
+    }
+}
